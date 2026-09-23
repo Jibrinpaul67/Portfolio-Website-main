3de9aa307920b2decaf851f7d61b887b9da6cda2
Esther Jibrin-Paul <jibriinesther@gmail.com>
2026-09-23 23:25:30 +0100

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ayanmahata.pages.dev',
+  site: 'https://jibrin.pages.dev',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
